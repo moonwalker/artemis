a93b1f3dd07485c4976ce73c7679ed48db131210
Kalman Speier <kalman.speier@gmail.com>
2022-11-17 17:38:45 +0100

    more config entries as ideas

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 export default {
-  MOONBASE_API_URL: 'http://localhost:4000'
+  MOONBASE_API_URL: 'http://localhost:4000',
+  GITHUB_ORG: 'moonwalker',
+  REPO_SLUG: 'moonwalker/casinohunt',
+  REPO_BRANCH: '' //# if empty this will default to main
 }
