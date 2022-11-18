7341114270a7db5610d2d187c6c7c667f29796de
rucsi <rucsi6@gmail.com>
2022-11-18 17:10:25 -0500

    popup window login

--- src/components/auth/index.jsx
+++ src/components/auth/index.jsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { useState, useEffect, Fragment } from 'react'
-import { useNavigate } from "react-router-dom"
-import { Dialog, Transition } from '@headlessui/react'
+import { useState, useEffect } from 'react'
 
 export function UserData() {
   const [data, setData] = useState()
@@ -23,10 +21,9 @@ export function UserData() {
 }
 
 export function Logout() {
-  const navigate = useNavigate()
-
   const signOut = () => {
-    navigate('/api/auth/signout')
+    localStorage.removeItem("artemis_token")
+    localStorage.removeItem("artemis_user")
   }
 
   return (
@@ -46,62 +43,30 @@ export function Logout() {
 }
 
 export function GithubAuthBox() {
-  const [iframeSrc, setIframeSrc] = useState()
-  function signIn() {
-    setIframeSrc(import.meta.env.VITE_MOONBASE_API_URL + "/login/github")
+  const redirectUrl = import.meta.env.VITE_MOONBASE_API_URL + "/login/github"
+  function signIn(e) {
+    e.preventDefault()
+    window.open(redirectUrl, 'popup', 'width=600,height=600,scrollbars=no,resizable=no')
+    window.addEventListener('message', (event) => {
+      if (event.data?.gh_token) {
+        localStorage.setItem("artemis_token", event.data.gh_token)
+        if (event.data?.artms_user) {
+          localStorage.setItem("artemis_user", atob(event.data.artms_user))
+        }
+      }
+    })
+    return false
   }
   return (
     <div class="w-full max-w-md space-y-8">
-      <button type="button" class="group relative flex w-full justify-center rounded-md border py-2 px-4 text-sm text-zinc-700 font-medium shadow-sm shadow-zinc-300 hover:bg-zinc-600 hover:text-zinc-100 focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2" onClick={signIn}>
+      <a class="group relative flex w-full justify-center rounded-md border py-2 px-4 text-sm text-zinc-700 font-medium shadow-sm shadow-zinc-300 hover:bg-zinc-600 hover:text-zinc-100 focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2" href={redirectUrl} target="popup" onClick={signIn}>
         <span class="flex items-center mr-4">
-          <svg class="w-5 fill-zinc-700 group-hover:fill-zinc-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+          <svg className="w-5 fill-zinc-700 group-hover:fill-zinc-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
             <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
           </svg>
         </span>
         <span class="hidden sm:inline-block">Sign in with Github</span>
-      </button>
-      <Dialog
-        open={!!iframeSrc}
-        onClose={() => false}
-        className="relative z-50"
-      >
-        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-
-        <div className="fixed inset-0 flex items-center justify-center p-4">
-          <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
-            <iframe class="absolute inset-0 w-full h-full" src={iframeSrc} allowfullscreen="true" frameborder="0"></iframe>
-            {/* <a class="group relative flex w-full justify-center rounded-md border py-2 px-4 text-sm text-zinc-700 font-medium shadow-sm shadow-zinc-300 hover:bg-zinc-600 hover:text-zinc-100 focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2" href={iframeSrc} target="_blank">
-              <span class="flex items-center mr-4">
-                <svg class="w-5 fill-zinc-700 group-hover:fill-zinc-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
-                  <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
-                </svg>
-              </span>
-              <span class="hidden sm:inline-block">Sign in with Github</span>
-            </a> */}
-          </Dialog.Panel>
-        </div>
-      </Dialog>
-      {/* <Transition
-        show={!!iframeSrc}
-        enter="transition duration-100 ease-out"
-        enterFrom="transform scale-95 opacity-0"
-        enterTo="transform scale-100 opacity-100"
-        leave="transition duration-75 ease-out"
-        leaveFrom="transform scale-100 opacity-100"
-        leaveTo="transform scale-95 opacity-0"
-        as={Fragment}
-      >
-        <Dialog onClose={() => false} className="relative z-50">
-          <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-          <div className="fixed inset-0 flex items-center justify-center p-4">
-            <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
-              <Dialog.Title>Deactivate account</Dialog.Title>
-
-              <iframe class="absolute inset-0 w-full h-full" src={iframeSrc} allowfullscreen="" frameborder="0"></iframe>
-            </Dialog.Panel>
-          </div>
-        </Dialog>
-      </Transition> */}
+      </a>
     </div >
   )
 }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -4,7 +4,7 @@ import networkImage from '../../assets/network-offline.png'
 export default () => (<div class="flex items-center justify-center h-screen">
     <div class="rounded-lg border shadow-lg p-10">
         <div>
-            <img class="mx-auto w-auto" src={networkImage} alt="Artemis" />
+            <img className="mx-auto w-auto" src={networkImage} alt="Artemis" />
         </div>
         <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
             <GithubAuthBox />
