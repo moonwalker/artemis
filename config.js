b0bca8b7ce112970ddbd592cadf2b465cec7f44c
Kalman Speier <kalman.speier@gmail.com>
2022-11-18 14:42:18 +0100

    env local example

--- config.js
+++
@@ -1,3 +0,0 @@
-export default {
-  MOONBASE_API_URL: 'http://localhost:4000'
-}
--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Logout from './components/logout'
 import { createClient } from '../lib/moonbase'
 
 export default function App() {
-  const client = createClient(config.MOONBASE_API_URL)
+  const client = createClient(import.meta.env.VITE_MOONBASE_API_URL)
 
   // client.getCollections(...), etc.
 
-  return <div class="container mx-auto">
-    <main role="main" >
-      <Login />
-      <Logout />
-    </main>
-  </div>
+  return (
+    <div class="container mx-auto">
+      <main role="main">
+        <Login />
+        <Logout />
+      </main>
+    </div>
+  )
 }
