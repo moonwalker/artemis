e4d44cd7e36edda0a6ac77fc862dc304e153716c
rucsi <rucsi6@gmail.com>
2022-11-22 11:19:16 -0500

    sha

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="p-4 bg-zinc-100 shadow-inner fixed inset-x-0 bottom-0 md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
             <span className="text-sm text-gray-600 sm:text-center dark:text-slate-400">© Moon & Walker Technologies 2022 | <a href="https://artemis.mw.zone/" className="text:zinc-600 hover:underline">Artemis</a>
             </span>
-            <span className="text-sm text-gray-600 sm:display-none dark:text-slate-400 float-right">@{commit}</span>
+            <span className="text-sm text-gray-600 sm:display-none dark:text-slate-400 float-right">{commit}</span>
         </footer>
     )
 }
