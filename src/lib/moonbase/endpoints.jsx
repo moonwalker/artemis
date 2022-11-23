5fb7e5dab1f9cce3f6eb747bc901a74ba2f6b799
rucsi <rucsi6@gmail.com>
2022-11-23 17:32:25 -0500

    cms endpoints

--- src/lib/moonbase/endpoints.jsx
+++ src/lib/moonbase/endpoints.jsx
@@ -1,7 +1,10 @@
 export const endpoints = {
     repos: () => ("/repos"),
     branches: (owner, repo) => (`/repos/${owner}/${repo}/branches`),
-    branch: (owner, repo, branch) => (`/repos/${owner}/${repo}/tree/${branch}`),
     tree: (owner, repo, branch, path) => (`/repos/${owner}/${repo}/tree/${branch}/${path}`),
     blob: (owner, repo, branch, path) => (`/repos/${owner}/${repo}/blob/${branch}/${path}`),
+
+    collections: (owner, repo, branch) => (`/cms/${owner}/${repo}/${branch}`),
+    documents: (owner, repo, branch, collection) => (`${this.collections(owner, repo, branch)}/${collection}`),
+    document: (owner, repo, branch, collection, document) => (`${this.documents(owner, repo, branch, collection)}/${document}`),
 }
