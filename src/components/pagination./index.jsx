0c0ac52c158cbfecd6b16365878e340c8d6a94ef
rucsi <rucsi6@gmail.com>
2022-11-25 11:08:27 -0500

    pagination

--- src/components/pagination./index.jsx
+++ src/components/pagination./index.jsx
@@ -12,6 +12,12 @@ export default ({ count = 0, lastPage = 1 }) => {
     const from = (page - 1) * per_page + 1
     const to = (page - 1) * per_page + count
 
+    if (!count)
+        return null
+
+    if (!lastPage)
+        lastPage = page
+
     return <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
         <div className="flex flex-1 justify-between sm:hidden">
             {page > 1 && <Link to={`${pathname}?page=${page - 1}`} className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</Link>}
@@ -36,7 +42,7 @@ export default ({ count = 0, lastPage = 1 }) => {
                             <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
                         </svg>
                     </Link>}
-                    <Link to={pathname} className={"relative z-10 inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20 " + (page == 1 ? activeClass : inactiveClass)}>1</Link>
+                    <Link to={pathname} className={"relative z-10 inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20 " + (page == 1 ? activeClass + " rounded-l-md" : inactiveClass)}>1</Link>
                     {lastPage > 1 && <Link to={`${pathname}?page=2`} className={"relative z-10 inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20 " + (page == 2 ? activeClass : inactiveClass)}>2</Link>}
                     {/* <a href="#" className="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 md:inline-flex">3</a> */}
                     {lastPage > 4 && <>
@@ -48,7 +54,7 @@ export default ({ count = 0, lastPage = 1 }) => {
                     </>}
                     {/* <a href="#" className="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 md:inline-flex">8</a> */}
                     {lastPage > 3 && <Link to={`${pathname}?page=${lastPage - 1}`} className={"relative z-10 inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20 " + (page == lastPage - 1 ? activeClass : inactiveClass)}>{lastPage - 1}</Link>}
-                    {lastPage > 2 && <Link to={`${pathname}?page=${lastPage}`} className={"relative z-10 inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20 " + (page == lastPage ? activeClass : inactiveClass)}>{lastPage}</Link>}
+                    {lastPage > 2 && <Link to={`${pathname}?page=${lastPage}`} className={"relative z-10 inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20 " + (page == lastPage ? activeClass + " rounded-r-md" : inactiveClass)}>{lastPage}</Link>}
                     {page < lastPage && <Link to={`${pathname}?page=${page + 1}`} className="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">
                         <span className="sr-only">Next</span>
                         <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
