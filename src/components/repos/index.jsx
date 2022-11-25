7c283a0965a5df676ab5a0c1111447442133720c
rucsi <rucsi6@gmail.com>
2022-11-25 11:08:00 -0500

    repos redirect to collections

--- src/components/repos/index.jsx
+++ src/components/repos/index.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { useClient, endpoints } from '../../lib/moonbase'
-import Error from '../../components/error'
-import Loader from '../../components/loader'
-import Pagination from '../../components/pagination'
+import Error from '../error'
+import Loader from '../loader'
+import Pagination from '../pagination.'
 
 export default () => {
     const client = useClient()
@@ -28,7 +28,7 @@ export default () => {
                 {!repos && !error && <Loader color="text-zinc-700" />}
                 {!!error && <Error error={error} />}
                 {!!repos && <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8">
-                    {(repos.items || []).map(repo => (<Link key={`${repo.owner}/${repo.name}`} to={`/${repo.owner}/${repo.name}/tree/${repo.defaultBranch}/`} className="p-4 border border-stone-200 rounded-md shadow-md flex items-center justify-center hover:shadow-xl hover-border-stone-400 focus:outline-none focus:ring focus:ring-stone-600 active:bg-stone-200 active:shadow-inner transition-all">
+                    {(repos.items || []).map(repo => (<Link key={`${repo.owner}/${repo.name}`} to={`/cms/${repo.owner}/${repo.name}/${repo.defaultBranch}/`} className="p-4 border border-stone-200 rounded-md shadow-md flex items-center justify-center hover:shadow-xl hover-border-stone-400 focus:outline-none focus:ring focus:ring-stone-600 active:bg-stone-200 active:shadow-inner transition-all">
                         {`${repo.owner}/${repo.name}`}
                     </Link>))}
                 </div>}
