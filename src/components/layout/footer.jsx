71a52d6cad29f5c22161257331dfb932584250b3
Kalman Speier <kalman.speier@gmail.com>
2022-11-30 16:24:39 +0100

    commit link

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,10 +1,10 @@
 export default function Footer() {
-    const commit = (__APP_ENV__.COMMIT_SHA || (__APP_ENV__.mode === 'development' ? 'dev' : '')).substring(0, 7)
+    const commit = '7a2d2f0'//(__APP_ENV__.COMMIT_SHA || (__APP_ENV__.mode === 'development' ? 'dev' : '')).substring(0, 7)
 
     return (
         <footer className="p-4 bg-zinc-100 shadow-inner fixed inset-x-0 bottom-0 flex items-center justify-center md:p-6 dark:bg-gray-800">
-            <span className="text-sm text-gray-600 sm:text-center dark:text-slate-400 mr-1">© Moon & Walker Technologies 2022</span>
-            {commit && <span className="text-sm text-gray-600 sm:display-none dark:text-slate-400">| {commit}</span>}
+            <span className="text-sm text-gray-600 sm:text-center dark:text-slate-400 mr-1">&copy; Moon & Walker Technologies 2022</span>
+            {commit && <span className="text-sm text-gray-600 sm:display-none dark:text-slate-400">| <a href={`https://github.com/moonwalker/artemis/commit/${commit}`} target="_blank">{commit}</a></span>}
         </footer>
     )
 }
