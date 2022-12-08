056da2b0f64953835eb4ba5a62c17504c68f4c31
Kalman Speier <kalman.speier@gmail.com>
2022-12-08 18:40:21 +0100

    react package fix

---
+++ packages/react/src/markdown.jsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { useState, useEffect, createElement } from 'react'
+import { MDXRemote } from 'next-mdx-remote'
+import { serialize } from 'next-mdx-remote/serialize'
+import remarkGfm from 'remark-gfm'
+
+import { useArtemis } from './hooks'
+
+const serializeOptions = {
+  mdxOptions: {
+    remarkPlugins: [remarkGfm]
+  }
+}
+
+export function MarkdownContent({ content, components, scope }) {
+  const [source, setSource] = useState()
+
+  const { data } = useArtemis({
+    data: content
+  })
+
+  const compile = async () => {
+    const s = await serialize(data, serializeOptions)
+    setSource(s)
+  }
+
+  useEffect(() => {
+    compile()
+  }, [data])
+
+  if (!source) {
+    return null
+  }
+
+  return createElement(MDXRemote, {
+    ...source,
+    components: components,
+    scope: scope
+  })
+}
