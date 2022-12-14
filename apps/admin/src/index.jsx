f351eb684efe1c0adafd8ac907060bab556d3395
Kalman Speier <kalman.speier@gmail.com>
2022-12-14 17:17:16 +0100

    admin app deps and build

--- apps/admin/src/index.jsx
+++ apps/admin/src/index.jsx
@@ -1,4 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import { AdminPage } from './app'
+import AdminPage from './app'
 
 createRoot(document.getElementById('root')).render(<AdminPage />)
--- apps/admin/vite.config.js
+++ apps/admin/vite.config.js
@@ -1,20 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig(() => {
   return {
     base: '/admin',
     plugins: [react()],
     build: {
       target: 'esnext',
       assetsDir: '.'
-    },
-    define: {
-      __APP_ENV__: {
-        MODE: mode,
-        TEST_APP_URL: env.TEST_APP_URL
-      }
     }
   }
 })
